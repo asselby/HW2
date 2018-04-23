fdc070b18ca24f91ea030bf78c12391ad526de80
asselby <asselid@bk.ru>
2018-04-23 16:44:44 +0600

    Добавьте файлы проекта.

---
+++ Server/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChatServerLogic.StartListening();
+            Console.ReadLine();
+        }
+    }
+}
